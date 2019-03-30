f493649191952dd6b5a317ec702ab2cd28ff1ab5
shalam15 <salamajayi@yahoo.com>
2019-03-30 05:10:28 -0400

    we completed the memlon games, some minor tweaks left

---
+++ javascript/scripts.js
@@ -0,0 +1,32 @@
+
+
+    function changeImage() {
+        let img =Math.floor(Math.random()*22+1).toString()
+        console.log(img)
+        document.getElementById("meme-container").src = "assets/"+img+".jpg";
+        
+    }
+
+    function changeImage1() {
+        let img =Math.floor(Math.random()*22+1).toString()
+        console.log(img)
+        document.getElementById("meme-container1").src = "assets/"+img+".jpg";
+        
+    }
+
+
+    function changeImage2() {
+        let img =Math.floor(Math.random()*22+1).toString()
+        console.log(img)
+        document.getElementById("meme-container2").src = "assets/"+img+".jpg";
+        
+    }
+
+
+    function changeImage3() {
+        let img =Math.floor(Math.random()*22+1).toString()
+        console.log(img)
+        document.getElementById("meme-container3").src = "assets/"+img+".jpg";
+        
+    }
+
